3a9f766d197b26fe098f52ac3dbcb46f2243979a
Ivan Sala <slavni96@gmail.com>
2023-04-08 14:53:28 +0000

    ;

--- configuration.nix
+++ configuration.nix
@@ -111,8 +111,8 @@
     slavni = {
       hashedPassword = "$y$j9T$ZQcBEEq/JtPjHIMNfT.R10$3txdoeaGHQtsKvxZz/z/2LtCsG.A1isj9r5HSI.utqD";
       inherit (openssh.authorizedKeys) keys;
-    }
-  }
+    };
+  };
 
 
   # Open ports in the firewall.
