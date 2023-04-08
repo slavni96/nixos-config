242aeb9214b62820c7d627c882cabdf2fb8a35be
Ivan Sala <slavni96@gmail.com>
2023-04-08 14:55:14 +0000

    openssh.authorizedKeys.keys = keys

--- configuration.nix
+++ configuration.nix
@@ -106,11 +106,11 @@
   {
     root = {
             password = "password";
-            inherit (openssh.authorizedKeys) keys;
+            openssh.authorizedKeys.keys = keys
           };
     slavni = {
       hashedPassword = "$y$j9T$ZQcBEEq/JtPjHIMNfT.R10$3txdoeaGHQtsKvxZz/z/2LtCsG.A1isj9r5HSI.utqD";
-      inherit (openssh.authorizedKeys) keys;
+      openssh.authorizedKeys.keys = keys
     };
   };
 
